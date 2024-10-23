7837e614e4c78e876f5d9302683838191bd27ad2
GloryMenga <glory.menga@student.ehb.be>
2024-10-23 15:51:10 +0200

    Concept shoe done

--- 3d-app/src/App.jsx
+++ 3d-app/src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import CanvasEffect from './components/CanvasEffect.jsx';
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <div className="App">
-        <CanvasEffect />
+      <div className='intro'>
+        <div className='left-intro'>
+            <h1>AIR JORDAN 1</h1>
+            <h2>MICHAEL JORDAN</h2>
+            <p>Scroll to see more about this model</p>
+        </div>
       </div>
     </>
   )

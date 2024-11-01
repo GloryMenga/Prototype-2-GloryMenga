5fd39b42fe1971cf0f6c58d64f43273d9d8d4203
GloryMenga <glory.menga@student.ehb.be>
2024-11-01 19:18:51 +0100

    3D object downloaded

--- 3d-app/src/App.jsx
+++ 3d-app/src/App.jsx
@@ -1,16 +1,11 @@
 import './App.css'
+import Home from './pages/Home.jsx';
 
 function App() {
 
   return (
     <>
-      <div className='intro'>
-        <div className='left-intro'>
-            <h1>AIR JORDAN 1</h1>
-            <h2>MICHAEL JORDAN</h2>
-            <p>Scroll to see more about this model</p>
-        </div>
-      </div>
+      <Home/>
     </>
   )
 }
---
+++ 3d-app/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+
+function Home() {
+
+  return (
+    <>
+      <div className='intro'>
+        <div className='left-intro'>
+            <h1>AIR JORDAN 1</h1>
+            <h2>MICHAEL JORDAN</h2>
+            <p>Scroll to see more about this model</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
